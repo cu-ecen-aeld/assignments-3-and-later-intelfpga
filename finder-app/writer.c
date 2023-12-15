eb4c50c62c2db504959b264deeb4644822e7bed1
intelfpga <intelfpga@proton.me>
2023-12-15 20:27:46 +0530

    Assignment 2 complete

---
+++ finder-app/writer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[]){
+
+	openlog(argv[0], LOG_PID, LOG_USER);
+	
+	if(argc != 3){
+		syslog(LOG_ERR, "wrong number of args:%d", argc);
+		exit(1);
+	}
+	
+	FILE *file = fopen(argv[1], "w");
+	
+	if(file == NULL){
+		syslog(LOG_DEBUG, "file:<%s> could not be opened", argv[1]);
+		return -1;
+	}else{
+	
+		fputs(argv[2], file);
+		syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+	}
+	fclose(file);
+	return 0;
+	
+	
+}
