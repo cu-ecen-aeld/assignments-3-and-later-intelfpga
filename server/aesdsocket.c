45ad7c6262b2e7eb30c13e4eccd01934ecd84ff7
intelfpga <intelfpga@proton.me>
2024-01-30 23:26:48 +0530

    fixed port reuse issue

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -20,7 +20,7 @@
 
 #define PORT "9000"
 #define SERVER_LOG "/var/tmp/aesdsocketdata"
-#define BUF_SIZE 100000
+#define BUF_SIZE 1000000
 
 struct  addrinfo *servinfo;
 int sock_fd, con_fd;
@@ -38,6 +38,7 @@ int main (int argc, char *argv[]){
 	
 	char str_addr[INET6_ADDRSTRLEN];
 	int rv;
+	int peek = 1;
 	
 		
 	memset(buf, 0, BUF_SIZE);
@@ -45,6 +46,8 @@ int main (int argc, char *argv[]){
 	memset(&hints, 0, sizeof (hints));
 	
 	openlog(argv[0], LOG_PID, LOG_USER); 
+	register_signals();
+	
 	
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
@@ -56,17 +59,28 @@ int main (int argc, char *argv[]){
 			   servinfo->ai_socktype,
 			   servinfo->ai_protocol);
 			   
+	if(setsockopt(sock_fd, SOL_SOCKET,SO_REUSEADDR, &peek, sizeof (peek))== -1)
+		printf("setsocket failed \n");
+			   
+				   
 	if (bind(sock_fd, servinfo->ai_addr, servinfo->ai_addrlen) != 0){
+		shutdown(sock_fd, SHUT_RDWR);
 		freeaddrinfo(servinfo);
+		close(sock_fd);
+		printf("socket could not bind to port \n");
 		return -1;
 	}
-		
+	
 	if(argc >1 && strcmp(argv[1],"-d") == 0)
 		daemon(1,1);
+	
+	
 		
 	listen(sock_fd, 10);
 	
 	int fd_w, fd_r;
+	
+	
 	
 	while(1){
 	
@@ -83,7 +97,7 @@ int main (int argc, char *argv[]){
 			   sizeof (str_addr));
 		
 		syslog(LOG_ERR, "Accepted connection from %s \n", str_addr);
-		
+						
 		receive_line(con_fd, buf, BUF_SIZE);
 		
 		write_line(fd_w, buf);
@@ -102,8 +116,8 @@ int main (int argc, char *argv[]){
 		
 	}
 	
-	freeaddrinfo(servinfo);
-	close(sock_fd);
+	//freeaddrinfo(servinfo);
+	//close(sock_fd);
 	//return 0;
 	
 }
@@ -124,12 +138,15 @@ static void sigint_handler(int signo){
 		
 		default:
 			syslog(LOG_ERR, "Caught unknown SIG \n");
-			freeaddrinfo(servinfo);
-			exit(1);
-		
+					
 	}
+
+	close(con_fd);
+	shutdown(sock_fd, SHUT_RDWR);
+	close(sock_fd);
 	freeaddrinfo(servinfo);
 	remove(SERVER_LOG);
+	//shutdown();
 	exit(0);
 
 }
